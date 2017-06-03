3ac73cf9c304839afd8d743b22954d7eefa67b83
Kevin Fleischman <kevinrfleischman@gmail.com>
2017-06-03 16:45:14 -0400

    adds attrs to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,10 @@
 class Image < ApplicationRecord
+  attr_accessor :name
+
+  belongs_to :imageable, polymorphic: true
+
+  has_attached_file :file, styles: {
+    medium: '400',
+    large: '800'
+  }
 end
